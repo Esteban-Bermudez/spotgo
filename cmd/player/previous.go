2dac15f243f52a9b48d9035a29cdea3d5f2d0b19
Esteban-Bermudez <esteban@bermudezaguirre.com>
2024-12-17 15:33:28 -0800

    Add next and previous sub commands to player
    This allows for skipping to the next or previous track in the current
    playback session.

---
+++ cmd/player/previous.go
@@ -0,0 +1,35 @@
+package player
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/Esteban-Bermudez/spotgo/cmd/connect"
+	"github.com/spf13/cobra"
+	"github.com/zmb3/spotify/v2"
+)
+
+var playerPreviousCmd = &cobra.Command{
+	Use:   "previous",
+	Short: "Skip to the previous track",
+	Long:  `Skip to the previous track in the current playback session`,
+	Run:   spotifyPrevious,
+}
+
+func init() {
+	playerCmd.AddCommand(playerPauseCmd)
+}
+
+func spotifyPrevious(cmd *cobra.Command, args []string) {
+	token, err := connect.LoadOAuthToken()
+	if err != nil {
+		log.Fatal("Error loading token, Run `spotgo connect` to connect to Spotify")
+	}
+
+	client := spotify.New(connect.Auth.Client(context.Background(), token))
+
+	client.Previous(context.Background())
+
+	fmt.Println("Previous track")
+}
