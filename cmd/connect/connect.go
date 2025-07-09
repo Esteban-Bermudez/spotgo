9878d4ed5ef51d3a361068838a59740e1b19e238
Esteban-Bermudez <esteban@bermudezaguirre.com>
2025-07-09 16:23:31 -0700

    Refactor the authentication process and add Viper
    This is a big commit that refactors the authentication process that was
    being done and separates it into a separate file to better manage
    creating a client from the same token stored in viper for other
    commands.
    
    I still need to implement a `goroutine` that will manage the refresh
    token and add some code to run before running any of my player sub
    commands.

--- cmd/connect/connect.go
+++ cmd/connect/connect.go
@@ -2,47 +2,25 @@ package connect
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/Esteban-Bermudez/spotgo/cmd/root"
-	"github.com/adrg/xdg"
-	"github.com/mozillazg/request"
+	"github.com/Esteban-Bermudez/spotgo/config"
 	"github.com/spf13/cobra"
-	"github.com/zmb3/spotify/v2"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 )
 
-// redirectURI is the OAuth redirect URI for the application.
-// You must register an application at Spotify's developer portal
-// and enter this value.
-const redirectURI = "http://localhost:8080/callback"
-
-// clientId is the client ID for the application. You must register
-// an application at Spotify's developer portal and enter this value.
-//
-// clientID might not be confidential and this application will be easier to run
-// if it does not require the user to get their own clientID.
 var (
-	clientId = os.Getenv("SPOTIFY_CLIENT_ID")
+	clientId = "762ca48c26614d3389d0c1337bc65d48" // Spotify client ID
 	Auth     = spotifyauth.New(
 		spotifyauth.WithClientID(clientId),
-		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithRedirectURL("http://localhost:8679/callback"),
 		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPlaybackState,
 			spotifyauth.ScopeUserReadCurrentlyPlaying,
 			spotifyauth.ScopeUserModifyPlaybackState))
-	tokenCh = make(chan *oauth2.Token)
 )
 
 var ConnectCmd = &cobra.Command{
@@ -57,192 +35,50 @@ func init() {
 }
 
 func connectToSpotify(cmd *cobra.Command, args []string) {
-	token, err := LoadOAuthToken()
-
+	v, err := config.InitConfig()
 	if err != nil {
-		if err.Error() == "Token not found" {
-			login()
-			token = <-tokenCh
-		} else if err.Error() == "Token expired" {
-			fmt.Println("Refreshing token...")
-			// token = updateToken(token)
-			token = RefreshToken(token)
-		} else {
-			log.Fatal(err)
-		}
+		log.Fatal("Error initializing config: ", err)
 	}
-	client := spotify.New(Auth.Client(context.Background(), token))
-	user, err := client.CurrentUser(context.Background())
+	fmt.Println("Using config file:", v.ConfigFileUsed())
+
+	ctx := context.Background()
+	spcli, err := config.SpotifyClient(ctx, v)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error getting Spotify client:", err)
 	}
-	fmt.Println("You are logged in as:", user.DisplayName)
-	saveOAuthToken(token)
-}
 
-func generateRandomString(size int) (string, error) {
-	possible := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	values := make([]byte, size)
-	_, err := rand.Read(values)
+	user, err := spcli.CurrentUser(ctx)
 	if err != nil {
-		return "", err
+		log.Fatal("Error getting current user:", err)
 	}
-	for i, b := range values {
-		values[i] = possible[int(b)%len(possible)]
-	}
-	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(values), nil
+	fmt.Println("Connected to Spotify as:", user.DisplayName)
 }
 
-func hashSha256(s string) string {
-	h := sha256.New()
-	h.Write([]byte(s))
-	bs := h.Sum(nil)
-	return string(bs)
-}
-
-func login() {
-	codeVerifier, err := generateRandomString(32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	codeChallenge := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString([]byte(hashSha256(codeVerifier)))
-	state, err := generateRandomString(16)
-	http.HandleFunc("/callback", completeAuth(state, codeVerifier))
-	go http.ListenAndServe(":8080", nil)
-
-	url := Auth.AuthURL(state,
-		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
-		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
-	)
-	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
-}
-
-func completeAuth(state string, codeVerifier string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		tok, err := Auth.Token(r.Context(), state, r,
-			oauth2.SetAuthURLParam("code_verifier", codeVerifier))
-		if err != nil {
-			http.Error(w, "Couldn't get token", http.StatusForbidden)
-			log.Fatal(err)
-		}
-		if st := r.FormValue("state"); st != state {
-			http.NotFound(w, r)
-			log.Fatalf("State mismatch: %s != %s\n", st, state)
-		}
-		w.Header().Set("Content-Type", "text/html")
-		fmt.Fprintf(w, "Login completed! You can now close this tab.")
-		tokenCh <- tok
-	}
-}
 func LoadOAuthToken() (*oauth2.Token, error) {
-	tokenFile, _ := xdg.SearchConfigFile("spotgo/token.json")
-	jsonToken, err := os.ReadFile(tokenFile)
+	v, err := config.InitConfig()
 	if err != nil {
-		return nil, fmt.Errorf("Token not found")
+		return nil, fmt.Errorf("error initializing config: %w", err)
 	}
 
-	var token oauth2.Token
-	if err := json.Unmarshal(jsonToken, &token); err != nil {
-		return nil, err
+	tok := v.GetStringMap("token")
+	if len(tok) == 0 {
+		return nil, fmt.Errorf("token not found")
+	}
+
+	exp, err := time.Parse(time.RFC3339, tok["expiry"].(string))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing token expiry: %w", err)
+	}
+	token := &oauth2.Token{
+		AccessToken:  tok["access_token"].(string),
+		TokenType:    tok["token_type"].(string),
+		RefreshToken: tok["refresh_token"].(string),
+		Expiry:       exp,
 	}
 
 	if token.Expiry.Before(time.Now()) {
-		return &token, fmt.Errorf("Token expired")
+		return nil, fmt.Errorf("token expired")
 	}
 
-	return &token, nil
-
-}
-
-func saveOAuthToken(token *oauth2.Token) error {
-	tokenFilePath, err := xdg.ConfigFile("spotgo/token.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	jsonToken, err := json.Marshal(token)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Saving token to", tokenFilePath)
-	return os.WriteFile(tokenFilePath, jsonToken, 0600)
-}
-
-func updateToken(token *oauth2.Token) *oauth2.Token {
-	refreshToken := token.RefreshToken
-	if refreshToken == "" {
-		log.Fatal("No refresh token found")
-	}
-
-	c := &http.Client{}
-	req := request.NewArgs(c)
-	req.Headers = map[string]string{
-		"Content-Type": "application/x-www-form-urlencoded",
-	}
-
-	req.Data = map[string]string{
-		"grant_type":    "refresh_token",
-		"refresh_token": refreshToken,
-		"client_id":     clientId,
-	}
-
-	resp, err := request.Post("https://accounts.spotify.com/api/token", req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(resp.Status)
-
-	content, err := resp.Content()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var newToken oauth2.Token
-	newToken.Expiry = time.Now().Add(time.Duration(3600) * time.Second)
-	if err := json.Unmarshal(content, &newToken); err != nil {
-		log.Fatal(err)
-	}
-	return &newToken
-
-}
-
-func RefreshToken(oldToken *oauth2.Token) *oauth2.Token {
-
-	form := url.Values{}
-	form.Add("grant_type", "refresh_token")
-	form.Add("refresh_token", oldToken.RefreshToken)
-	form.Add("client_id", clientId)
-
-	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", strings.NewReader(form.Encode()))
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-
-	var newToken oauth2.Token
-	if err := json.Unmarshal(body, &newToken); err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	if newToken.Expiry.IsZero() {
-		newToken.Expiry = time.Now().Add(time.Duration(3600) * time.Second)
-	}
-
-	return &newToken
+	return token, nil
 }
---
+++ config/config.go
@@ -0,0 +1,192 @@
+package config
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/pkg/browser"
+	"github.com/spf13/viper"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
+)
+
+func InitConfig() (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName("spotgo")
+	v.SetConfigType("json")
+	v.AddConfigPath(os.ExpandEnv("$HOME/.config/spotgo/"))    // Default config directory
+	v.AddConfigPath(os.ExpandEnv("$XDG_CONFIG_HOME/spotgo/")) // Optional: XDG config directory
+	v.AddConfigPath(os.ExpandEnv("$HOME/.spotgo/"))           // Optional: Home directory
+
+	err := v.ReadInConfig()
+	var configFileNotFoundError *viper.ConfigFileNotFoundError
+	missingConfig := errors.As(err, configFileNotFoundError)
+	if missingConfig && v.ConfigFileUsed() == "" {
+		err = setDefaultConfig(v) // Set default values with a login prompt
+		if err != nil {
+			return nil, fmt.Errorf("failed to set default config: %w", err)
+		}
+		v.SetConfigFile(os.ExpandEnv("$HOME/.config/spotgo/spotgo.json"))
+		return v, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+	t, err := getOAuthToken(v) // Ensure the token is loaded
+	if err != nil {
+		return nil, fmt.Errorf("error getting OAuth token: %w", err)
+	}
+	_, err = RefreshAndSaveToken(t, v)
+	if err != nil {
+		return nil, fmt.Errorf("error refreshing and saving token: %w", err)
+	}
+	return v, nil
+}
+
+func RefreshAndSaveToken(t *oauth2.Token, v *viper.Viper) (*oauth2.Token, error) {
+	if t.Expiry.Before(time.Now()) {
+		auth := getAuthenticator()
+		token, err := auth.RefreshToken(context.Background(), t)
+		if err != nil {
+			return nil, fmt.Errorf("error refreshing token: %w", err)
+		}
+		tokenMap, err := MarshalToken(token)
+		if err != nil {
+			return nil, fmt.Errorf("error marshaling token: %w", err)
+		}
+		v.Set("token", tokenMap)
+		err = v.WriteConfig()
+		if err != nil {
+			return nil, fmt.Errorf("error writing config file: %w", err)
+		}
+		return token, nil
+	}
+	return t, nil
+}
+
+func getAuthenticator() *spotifyauth.Authenticator {
+	clientID := "762ca48c26614d3389d0c1337bc65d48"
+	auth := spotifyauth.New(
+		spotifyauth.WithClientID(clientID),
+		spotifyauth.WithRedirectURL("http://localhost:8679/callback"),
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPlaybackState,
+			spotifyauth.ScopeUserReadCurrentlyPlaying,
+			spotifyauth.ScopeUserModifyPlaybackState))
+
+	return auth
+}
+
+func setDefaultConfig(v *viper.Viper) error {
+	// login the user and create a token
+	codeVerifier, err := generateRandomString(32)
+	if err != nil {
+		return fmt.Errorf("failed to generate random code verifier: %w", err)
+	}
+
+	codeChallenge := base64.URLEncoding.WithPadding(base64.NoPadding).
+		EncodeToString([]byte(hashSha256(codeVerifier)))
+
+	state, err := generateRandomString(16)
+	if err != nil {
+		return fmt.Errorf("failed to generate random state: %w", err)
+	}
+
+	auth := getAuthenticator()
+
+	url := auth.AuthURL(state,
+		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
+		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
+		oauth2.SetAuthURLParam("state", state),
+	)
+
+	ch := make(chan *spotify.Client)
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		t, err := auth.Token(r.Context(), state, r,
+			oauth2.SetAuthURLParam("code_verifier", codeVerifier))
+		if err != nil {
+			http.Error(w, "Couldn't get token", http.StatusForbidden)
+			log.Fatal(err)
+		}
+		if st := r.FormValue("state"); st != state {
+			http.NotFound(w, r)
+			log.Fatalf("State mismatch: %s != %s\n", st, state)
+		}
+		client := spotify.New(auth.Client(r.Context(), t))
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, "Login completed! You can now close this tab.")
+		ch <- client
+	})
+
+	go func() {
+		err := http.ListenAndServe(":8679", nil)
+		if err != nil {
+			log.Fatalf("Failed to start HTTP server: %v", err)
+		}
+	}()
+
+	fmt.Printf(
+		"Please log in to Spotify by visiting the following page in your browser: %s\n\n",
+		url,
+	)
+
+	browser.OpenURL(url)
+
+	client := <-ch
+	if client == nil {
+		return fmt.Errorf("failed to get client from channel")
+	}
+
+	token, err := client.Token()
+	if err != nil {
+		return fmt.Errorf("failed to get token from client: %w", err)
+	}
+	tokenMap := map[string]any{
+		"access_token":  token.AccessToken,
+		"token_type":    token.TokenType,
+		"refresh_token": token.RefreshToken,
+		"expiry":        token.Expiry.Format(time.RFC3339),
+	}
+	// Set the token in the viper configuration
+	v.Set("token", tokenMap)
+	return v.SafeWriteConfig()
+}
+
+func getOAuthToken(v *viper.Viper) (*oauth2.Token, error) {
+	tokenMap := v.GetStringMap("token")
+	if tokenMap == nil {
+		return nil, fmt.Errorf("no token found in config")
+	}
+	exp, err := time.Parse(time.RFC3339, tokenMap["expiry"].(string))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse token expiry: %w", err)
+	}
+	token := &oauth2.Token{
+		AccessToken:  tokenMap["access_token"].(string),
+		TokenType:    tokenMap["token_type"].(string),
+		RefreshToken: tokenMap["refresh_token"].(string),
+		Expiry:       exp,
+	}
+	return token, nil
+}
+
+func SpotifyClient(ctx context.Context, v *viper.Viper) (*spotify.Client, error) {
+	token, err := getOAuthToken(v)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get OAuth token: %w", err)
+	}
+
+	auth := getAuthenticator()
+	client := spotify.New(auth.Client(ctx, token))
+	if client == nil {
+		return nil, fmt.Errorf("failed to create Spotify client")
+	}
+	return client, nil
+}
